7e91c4c7193942c88de9ed7d3ef554f6f8818e16
Brenden <52762708+brendenmoore@users.noreply.github.com>
2020-05-08 16:44:28 -0400

    added form validation

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 // Write your JavaScript code here!
 
+window.addEventListener("load", function() {
+   let form = document.getElementById("launchForm");
+   form.addEventListener("submit", function(event) {
+      let pilot = document.getElementById("pilotName").value;
+      let copilot = form.querySelector("input[name=copilotName]").value;
+      let fuel = form.querySelector("input[name=fuelLevel]").value;
+      let cargo = form.querySelector("input[name=cargoMass]").value;
+
+      if (!isValidForm(pilot, copilot, fuel, cargo)) {
+         event.preventDefault();
+         alert("Invalid!");
+      }
+
+      let liPilotStatus = document.getElementById("pilotStatus");
+      let liCopilotStatus = document.getElementById("copilotStatus");
+      liPilotStatus.innerHTML = `${pilot} Ready`;
+      liCopilotStatus.innerHTML = `${copilot} Ready`;
+
+
+
+
+   });
+});
+
+function isValidForm(pilot, copilot, fuel, cargo) {
+   if (pilot === "" || copilot === "" || fuel === "" || cargo === "") {
+      return false;
+   } else if (isNaN(Number(fuel)) || isNaN(Number(cargo)) || !isNaN(Number(pilot)) || !isNaN(Number(copilot))) {
+      return false;
+   } else {
+      return true;
+   }
+}
+
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
