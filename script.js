9460956b80ba5180ce26d2a1bb92394d18ce81ba
Brenden <52762708+brendenmoore@users.noreply.github.com>
2020-05-09 11:34:54 -0400

    updating shuttle requirements

--- script.js
+++ script.js
@@ -8,18 +8,15 @@ window.addEventListener("load", function() {
       let fuel = form.querySelector("input[name=fuelLevel]").value;
       let cargo = form.querySelector("input[name=cargoMass]").value;
 
+
+
       if (!isValidForm(pilot, copilot, fuel, cargo)) {
-         event.preventDefault();
-         alert("Invalid!");
+         alert("Invalid Form!");
+      } else {
+         checkRequirements(pilot, copilot, fuel, cargo);
       }
 
-      let liPilotStatus = document.getElementById("pilotStatus");
-      let liCopilotStatus = document.getElementById("copilotStatus");
-      liPilotStatus.innerHTML = `${pilot} Ready`;
-      liCopilotStatus.innerHTML = `${copilot} Ready`;
-
-
-
+      event.preventDefault();
 
    });
 });
@@ -31,6 +28,42 @@ function isValidForm(pilot, copilot, fuel, cargo) {
       return false;
    } else {
       return true;
+   }
+}
+
+function checkRequirements(pilot, copilot, fuel, cargo) {
+   let liPilotStatus = document.getElementById("pilotStatus");
+   let liCopilotStatus = document.getElementById("copilotStatus");
+   let divFaultyItems = document.getElementById("faultyItems");
+   let h2LaunchStatus = document.getElementById("launchStatus");
+
+   divFaultyItems.style.visibility = "visible";
+   liPilotStatus.innerHTML = `${pilot} Ready`;
+   liCopilotStatus.innerHTML = `${copilot} Ready`;
+   let isReady = true;
+
+   let liFuelStatus = document.getElementById("fuelStatus");
+   if (fuel < 10000) {
+      liFuelStatus.innerHTML = "Not enough fuel.";
+      isReady = false;
+   } else {
+      liFuelStatus.innerHTML = "Fuel level high enough for launch";
+   }
+
+   let liCargoStatus = document.getElementById("cargoStatus");
+   if (cargo > 10000) {
+      liCargoStatus.innerHTML = "Too much mass for shuttle to take off.";
+      isReady = false;
+   } else {
+      liCargoStatus.innerHTML = "Cargo mass low enough for launch";
+   }
+
+   if (isReady) {
+      h2LaunchStatus.innerHTML = "Shuttle is ready for launch.";
+      h2LaunchStatus.style.color = "green";
+   } else {
+      h2LaunchStatus.innerHTML = "Shuttle not ready for launch.";
+      h2LaunchStatus.style.color = "red";
    }
 }
 
